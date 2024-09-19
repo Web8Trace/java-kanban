cb3ad3270be78a3a3e81f0698b227dbc85be10e3
Denis Ch. <chdennik@yandex.ru>
2024-09-19 21:23:07 +0300

    сделана ветка, добавлен метод в интерфес

--- src/com/github/web8trace/kanban/memory/InMemoryHistoryManager.java
+++ src/com/github/web8trace/kanban/memory/InMemoryHistoryManager.java
@@ -2,13 +2,18 @@ package com.github.web8trace.kanban.memory;
 
 import com.github.web8trace.kanban.model.Task;
 import com.github.web8trace.kanban.service.HistoryManager;
+import com.github.web8trace.kanban.service.Node;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
+    private Node head;
+    private Node tail;
+    private int size = 0;
 
-    private List<Task> history = new ArrayList<>();
+    private List<Task> history = new LinkedList<>();
+
+    private Map<Integer, Node> history1 = new HashMap<>();
 
     @Override
     public void add(Task task) {
@@ -16,6 +21,11 @@ public class InMemoryHistoryManager implements HistoryManager {
             history.remove(0);
         }
         history.add(task);
+    }
+
+    @Override
+    public void remove(int id) {
+
     }
 
     @Override
---
+++ src/com/github/web8trace/kanban/service/Node.java
@@ -0,0 +1,7 @@
+package com.github.web8trace.kanban.service;
+
+public class Node<T> {
+    public T data;
+    public Node<T> next;
+    public Node<T> prev;
+}
